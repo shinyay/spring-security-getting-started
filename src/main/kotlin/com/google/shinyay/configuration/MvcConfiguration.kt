c3f4c59f889d7ee1c83c31873609ab8f78fbf8b8
shinyay <shinya.com@gmail.com>
2020-08-29 22:23:57 +0900

    Add ViewController for Home View

--- src/main/kotlin/com/google/shinyay/configuration/MvcConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/MvcConfiguration.kt
@@ -1,7 +1,13 @@
 package com.google.shinyay.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class MvcConfiguration {
+class MvcConfiguration : WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/home").setViewName("home")
+        registry.addViewController("/").setViewName("home")
+    }
 }
