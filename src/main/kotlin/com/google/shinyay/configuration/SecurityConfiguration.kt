c74ec2a7ced9fa3ec31bc63a601bb95efea40ff0
shinyay <shinya.com@gmail.com>
2020-08-31 22:50:16 +0900

    Add override method userDetailService

--- src/main/kotlin/com/google/shinyay/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/SecurityConfiguration.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
 
 @Configuration
 @EnableWebSecurity
@@ -11,5 +12,9 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         super.configure(http)
+    }
+
+    override fun userDetailsService(): UserDetailsService {
+        return super.userDetailsService()
     }
 }
