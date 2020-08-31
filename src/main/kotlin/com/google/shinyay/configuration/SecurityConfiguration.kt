6fe1ef2cfef2d89aab26dbcb2db7894ebec17fe4
shinyay <shinya.com@gmail.com>
2020-08-31 22:49:28 +0900

    Add override method configue

--- src/main/kotlin/com/google/shinyay/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/SecurityConfiguration.kt
@@ -1,10 +1,15 @@
 package com.google.shinyay.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        super.configure(http)
+    }
 }
