1fb29e5c542a55a3fc560845adfb6bf0639190ee
shinyay <shinya.com@gmail.com>
2020-09-05 22:14:13 +0900

    Add view controller for LOGIN

--- src/main/kotlin/com/google/shinyay/configuration/MvcConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/MvcConfiguration.kt
@@ -10,5 +10,6 @@ class MvcConfiguration : WebMvcConfigurer {
         registry.addViewController("/home").setViewName("home")
         registry.addViewController("/").setViewName("home")
         registry.addViewController("/hello").setViewName("hello")
+        registry.addViewController("/login").setViewName("login")
     }
 }
